15725a835356b202ddb0b24b1ceb4f883ae83232
Saksham Bindal <saksham.bindal2004@gmail.com>
2022-12-12 03:37:30 +0530

    bootstrap script fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 import React from 'react'
 
 const Document = () => {
@@ -12,7 +11,7 @@ const Document = () => {
             <body>
                 <Main />
                 <NextScript />
-                <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous" />
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous" async />
             </body>
         </Html>
     )
